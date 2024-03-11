deba0ccd1d382229cd4f187b19753c838790a6d6
Yi <xyxy0202@qq.com>
2024-03-11 10:29:47 +0800

    request

---
+++ request.go
@@ -0,0 +1,54 @@
+package rego
+
+import (
+	"context"
+	"sync"
+)
+
+type Request[T, V any] struct {
+	msg  T
+	ok   *V
+	err  error
+	ch   chan struct{}
+	once sync.Once
+}
+
+func NewRequest[T, V any](msg T) *Request[T, V] {
+	return &Request[T, V]{
+		msg,
+		nil,
+		nil,
+		make(chan struct{}),
+		sync.Once{},
+	}
+}
+
+func (req *Request[T, V]) Msg() T {
+	return req.msg
+}
+
+func (req *Request[T, V]) Resolve(value V) {
+	req.once.Do(func() {
+		req.ok = &value
+		close(req.ch)
+	})
+}
+
+func (req *Request[T, V]) Reject(err error) {
+	req.once.Do(func() {
+		req.err = err
+		close(req.ch)
+	})
+}
+
+func (req *Request[T, V]) Await(ctx context.Context) (ok V, err error) {
+	select {
+	case <-ctx.Done():
+		err = ctx.Err()
+		return
+	case <-req.ch:
+		ok = *req.ok
+		err = req.err
+		return
+	}
+}
--- sink.go
+++ sink.go
@@ -6,11 +6,12 @@ import (
 )
 
 type Sink[T any] struct {
-	ch chan T
+	ctx context.Context
+	ch  chan T
 }
 
-func (s *Sink[T]) Write(value T, timeout time.Duration) error {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+func (s *Sink[T]) WriteWithTimeout(value T, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(s.ctx, timeout)
 	defer cancel()
 	select {
 	case <-ctx.Done():
@@ -20,8 +21,21 @@ func (s *Sink[T]) Write(value T, timeout time.Duration) error {
 	}
 }
 
-func (s *Sink[T]) WriteWithCtx(ctx context.Context, value T) error {
+func (s *Sink[T]) Write(value T) error {
 	select {
+	case <-s.ctx.Done():
+		return s.ctx.Err()
+	case s.ch <- value:
+		return nil
+	}
+}
+
+func (s *Sink[T]) WriteWithCtx(ctx context.Context, value T) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	select {
+	case <-s.ctx.Done():
+		return s.ctx.Err()
 	case <-ctx.Done():
 		return ctx.Err()
 	case s.ch <- value:
@@ -29,9 +43,10 @@ func (s *Sink[T]) WriteWithCtx(ctx context.Context, value T) error {
 	}
 }
 
-func NewSink[T any](size int) (*Sink[T], <-chan T) {
+func NewSink[T any](ctx context.Context, size int) (*Sink[T], <-chan T) {
 	ch := make(chan T, size)
 	return &Sink[T]{
+		ctx,
 		ch,
 	}, ch
 }
