6fe437ed0cb480e06b0c980c053d983f60f44b6d
Yi <xyxy0202@qq.com>
2024-07-30 15:33:19 +0800

    NewStream

---
+++ stream.go
@@ -0,0 +1,84 @@
+package rego
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+type Stream[T any] struct {
+	ctx       context.Context
+	handler   Handler
+	subChan   chan *subscription[T]
+	sink      *Sink[T]
+	once      *sync.Once
+	generator func(ctx context.Context, sink *Sink[T])
+	sinkSize  int
+}
+
+type subscription[T any] struct {
+	key     string
+	writeCh chan<- T
+	take    int
+	taken   int
+}
+
+func NewStream[T any](ctx context.Context, sinkSize int, generator func(ctx context.Context, sink *Sink[T])) *Stream[T] {
+	subChan := make(chan *subscription[T], 1)
+	sink, sinkCh := NewSink[T](ctx, sinkSize)
+	handler := func(ctx context.Context) (err error) {
+		subMap := map[string]*subscription[T]{}
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case sub := <-subChan:
+				if sub.writeCh == nil {
+					delete(subMap, sub.key)
+					continue
+				}
+				subMap[sub.key] = sub
+			case data := <-sinkCh:
+				for _, sub := range subMap {
+					if sub.take != 0 && sub.take == sub.taken {
+						delete(subMap, sub.key)
+						continue
+					}
+					sub.writeCh <- data
+					sub.taken++
+				}
+			}
+		}
+	}
+	return &Stream[T]{
+		ctx,
+		handler,
+		subChan,
+		sink,
+		&sync.Once{},
+		generator,
+		sinkSize,
+	}
+}
+
+func (s *Stream[T]) Start() (co *Coroutine) {
+	s.once.Do(func() {
+		co = Go(s.ctx, s.handler)
+		s.generator(s.ctx, s.sink)
+	})
+	return
+}
+
+// count 0: infinite
+func (s *Stream[T]) Take(count int) (result <-chan T) {
+	ch := make(chan T, s.sinkSize)
+	result = ch
+	key := fmt.Sprintf("%p", result)
+	s.subChan <- &subscription[T]{
+		key,
+		ch,
+		count,
+		0,
+	}
+	return
+}
